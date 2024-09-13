7dc46964e3f2d771b5a7d26018ba92d5d98e78d3
abazdev34 <abaztoktorbaev89@gmail.com>
2024-09-13 09:25:18 +0300

    allResults

--- src/pages/admin/studentsResults.tsx
+++ src/pages/admin/studentsResults.tsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllResults } from '../../redux/features/question/questionSlice'; // Adjust the import path as necessary
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllResults } from '../../redux/features/question/questionSlice' // Adjust the import path as necessary
 
 const StudentsResults = () => {
-    const dispatch = useDispatch();
-    const { results, loading, error } = useSelector((state) => state.results); // Ensure this matches your Redux state structure
+	const dispatch = useDispatch()
+	const { allResults, loading, error } = useSelector((state) => state.questions) // Ensure this matches your Redux state structure
 
-    useEffect(() => {
-        dispatch(getAllResults());
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(getAllResults())
+	}, [dispatch])
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
+	if (loading) return <p>Loading...</p>
+	if (error) return <p>Error: {error}</p>
 
-    return (
-        <div>
-            <h1>Students Results</h1>
-            {results.length > 0 ? (
-                <ul>
-                    {results.map((result) => (
-                        <li key={result.id}>
-                            <p>Username: {result.username}</p>
-                            <p>Score: {result.score}</p>
-                            <p>Time Taken: {result.time} seconds</p>
-                            <p>Date: {result.date}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No results found.</p>
-            )}
-        </div>
-    );
-};
+	return (
+		<div>
+			<h1>Students Results</h1>
+			{allResults.items?.length > 0 ? (
+				<ul>
+					{allResults.items.map((result) => (
+						<li key={result.id}>
+							<p>Username: {result.student_id}</p>
+							{/* <p>Score: {result.answer}</p> */}
+							<p>Time Taken: {result.time} seconds</p>
+							{/* <p>Date: {result.date}</p> */}
+						</li>
+					))}
+				</ul>
+			) : (
+				<p>No results found.</p>
+			)}
+		</div>
+	)
+}
 
-export default StudentsResults;
+export default StudentsResults
